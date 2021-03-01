159aa0761916157bb7579c17b240c4212aad2dae
Isaac Mendez <IsaacMendez.work@gmail.com>
2021-03-01 08:02:56 -0500

    updated stuff for practice

--- main.js
+++ main.js
@@ -12,3 +12,14 @@ console.log(
   `your total, including gratutity is:\
 	$${totalWithGrat(billAmount).toFixed(2)}`
 );
+
+let billAmount = 111.11;
+
+function gratuity() {
+  return billAmount * 0.2;
+}
+
+function totalWithGrat(amount) {
+  return gratuity() + amount;
+}
+
