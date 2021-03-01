b9f8c75d3390a53eac0091c0b8a5372069923f50
Isaac Mendez <IsaacMendez.work@gmail.com>
2021-03-01 10:41:44 -0500

    added challenge 2

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-let billAmount = 111.11;
+/* let billAmount = 111.11;
 
 function gratuity() {
   return billAmount * 0.2;
@@ -8,6 +8,18 @@ function totalWithGrat(amount) {
   return gratuity() + amount;
 }
 
-console.log(
-  `Your total, including gratuity, is: $${totalWithGrat(billAmount).toFixed(2)}`
-);
+console.log( 
+  `Your total, including gratuity, is: $${totalWithGrat(billAmount).toFixed(2)}.`
+); */
+
+let handsArray = [
+  `rock`, `paper`, `scissors`;
+]
+
+function getComputerHand() {
+  return hands[parseInt(Math.random()*10) % 3];
+}
+
+function compare(handsArray) {}
+
+console.log(handsArray[compare])
